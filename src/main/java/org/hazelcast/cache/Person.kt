9440589e3b4e1d666389ac8c761877c62b269d2f
Nicolas Frankel <nicolas@frankel.ch>
2020-10-28 16:28:30 +0100

    Manually add caching in a naive blocking way

--- src/main/java/org/hazelcast/cache/Person.kt
+++ src/main/java/org/hazelcast/cache/Person.kt
@@ -17,22 +17,22 @@ import java.time.LocalDate
 class PersonRoutes {
 
     @Bean
-    fun router(repository: PersonRepository) = router {
-        val handler = PersonHandler(repository)
+    fun router(service: CachingService) = router {
+        val handler = PersonHandler(service)
         GET("/person", handler::getAll)
         GET("/person/{id}", handler::getOne)
     }
 }
 
-class PersonHandler(private val repository: PersonRepository) {
+class PersonHandler(private val service: CachingService) {
 
     fun getAll(req: ServerRequest): Mono<ServerResponse> {
-        val flux = repository.findAll(Sort.by("lastName", "firstName"))
+        val flux = service.findAll(Sort.by("lastName", "firstName"))
         return ok().body<Person>(flux)
     }
 
     fun getOne(req: ServerRequest): Mono<ServerResponse> {
-        val mono = repository
+        val mono = service
             .findById(req.pathVariable("id").toLong())
             .switchIfEmpty(Mono.error { ResponseStatusException(NOT_FOUND) })
         return ok().body<Person>(mono)
